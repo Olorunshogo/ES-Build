c83c8cf15dc182f94fdcaaf173c71da8e9c546a8
BAMTEFA Olorunshogo Moses <shownzy001@gmail.com>
2025-02-21 11:23:20 +0100

    First build.

--- build.mjs
+++ build.mjs
@@ -1,1 +1,20 @@
 
+import * as esbuild from 'esbuild';
+
+await esbuild.build({
+    entryPoints: ['app.jsx', 'app.ts'], // Include both .jsx and .ts file
+    bundle: true,
+    // outfile: 'out.js', // Output into a single file
+    outdir: 'dist', // Output into a directory instead of a single file
+    minify: true,
+    sourcemap: true,
+    platform: 'node',
+    target: ['chrome58', 'firefox57', 'safari11', 'edge16', 'node10.4'],
+    define: {
+        'process.env.NODE_ENV': '"production"',
+    },
+    loader: {
+        '.jsx': '.jsx',
+        '.ts': '.ts',
+   }
+});
